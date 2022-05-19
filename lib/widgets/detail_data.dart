e3cff6e0dc198bc5a6a5c2f3eab79bbf8f3d0082
professoremerson <emerson@unc.br>
2022-05-18 21:18:16 -0300

    Definição dos elementos para exibição de detalhes do Pokémon + inserção de assets

--- lib/widgets/detail_data.dart
+++ lib/widgets/detail_data.dart
@@ -7,26 +7,75 @@ class DetailData extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      constraints: BoxConstraints(
-        minWidth: MediaQuery.of(context).size.width,
-        minHeight: 500,
-      ),
-      decoration: const BoxDecoration(
-        color: Colors.white,
-        border: Border(
-          top: BorderSide(
-            width: 2,
-            color: Colors.grey,
+    return Align(
+      alignment: Alignment.bottomCenter,
+      child: Container(
+        height: MediaQuery.of(context).size.height,
+        width: MediaQuery.of(context).size.width,
+        decoration: BoxDecoration(
+          color: Colors.white,
+          borderRadius: BorderRadius.only(
+            topLeft: Radius.circular(50),
+            topRight: Radius.circular(50),
           ),
+          boxShadow: [
+            BoxShadow(
+                color: Colors.red.withOpacity(0.5),
+                offset: Offset(0, -5),
+                blurRadius: 8),
+          ],
         ),
-        borderRadius: BorderRadius.only(
-          topLeft: Radius.circular(71),
-          topRight: Radius.circular(71),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Padding(
+              padding: const EdgeInsets.only(
+                top: 20,
+                left: 30,
+                right: 30,
+              ),
+              child: Row(
+                children: [
+                  Expanded(
+                    child: Text(
+                      "Descrição",
+                      style: TextStyle(
+                          fontSize: 28,
+                          fontWeight: FontWeight.bold,
+                          color: Colors.black),
+                    ),
+                  ),
+                  InkWell(
+                    onTap: () {
+                      print("Clicou aqui!");
+                    },
+                    child: Image(
+                      height: 20,
+                      width: 20,
+                      image: AssetImage('heart_1.png'),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            Padding(
+              padding: EdgeInsets.all(40),
+              child: Row(
+                children: [
+                  Expanded(
+                    child: Text(
+                      "Você usa o Lorem Ipsum tradicional? Sabe de nada inocente!! Conheça meu lorem que é Tchan, Tchan, Tchannn!! Txu Txu Tu Paaaaa!!",
+                      style: TextStyle(
+                        fontSize: 20,
+                        color: Colors.black.withOpacity(0.5),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ],
         ),
-      ),
-      child: Column(
-        children: [],
       ),
     );
   }
